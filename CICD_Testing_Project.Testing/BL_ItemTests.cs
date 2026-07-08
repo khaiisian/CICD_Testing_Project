f48b28859d5b2bc8c94244e4d6d5e1fdc2929118
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-07-08 10:41:14 +0630

    add BL_Item unit test with Moq

---
+++ CICD_Testing_Project.Testing/BL_ItemTests.cs
@@ -0,0 +1,27 @@
+﻿using CICD_Testing_Project.Api.Domain.Features.Item;
+using CICD_Testing_Project.Api.Models.Features.Item;
+using Moq;
+
+namespace CICD_Testing_Project.Testing
+{
+    public class BL_ItemTests
+    {
+        [Fact]
+        public async Task GetByIdTest()
+        {
+            // Arrange
+            var mockDa = new Mock<IDA_Item>();
+            mockDa.Setup(x => x.GetById(1, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ItemResponseModel { Id = 1, Name = "Test Item" });
+
+            var bl = new BL_Item(mockDa.Object);
+
+            // Act
+            var result = await bl.GetById(1, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Test Item", result!.Name);
+        }
+    }
+}
