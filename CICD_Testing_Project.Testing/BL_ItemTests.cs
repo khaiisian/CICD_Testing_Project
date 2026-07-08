db8566e29483a37d2823377f6331943abcdd3299
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-07-08 13:58:15 +0630

    add CI pipeline

--- CICD_Testing_Project.Testing/BL_ItemTests.cs
+++ CICD_Testing_Project.Testing/BL_ItemTests.cs
@@ -2,26 +2,25 @@
 using CICD_Testing_Project.Api.Models.Features.Item;
 using Moq;
 
-namespace CICD_Testing_Project.Testing
+namespace CICD_Testing_Project.Testing;
+
+public class BL_ItemTests
 {
-    public class BL_ItemTests
+    [Fact]
+    public async Task GetByIdTest()
     {
-        [Fact]
-        public async Task GetByIdTest()
-        {
-            // Arrange
-            var mockDa = new Mock<IDA_Item>();
-            mockDa.Setup(x => x.GetById(1, It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new ItemResponseModel { Id = 1, Name = "Test Item" });
+        // Arrange
+        var mockDa = new Mock<IDA_Item>();
+        mockDa.Setup(x => x.GetById(1, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ItemResponseModel { Id = 1, Name = "Test Item" });
 
-            var bl = new BL_Item(mockDa.Object);
+        var bl = new BL_Item(mockDa.Object);
 
-            // Act
-            var result = await bl.GetById(1, CancellationToken.None);
+        // Act
+        var result = await bl.GetById(1, CancellationToken.None);
 
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal("Test Item", result!.Name);
-        }
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Item", result!.Name);
     }
 }
