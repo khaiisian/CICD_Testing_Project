d667007cc46693ee7f15e949c87ae401546ed5d7
Khai Sian <114007490+khaiisian@users.noreply.github.com>
2026-07-10 23:27:35 +0630

    Update BL_ItemTests.cs

--- CICD_Testing_Project.Testing/BL_ItemTests.cs
+++ CICD_Testing_Project.Testing/BL_ItemTests.cs
@@ -21,7 +21,7 @@ public class BL_ItemTests
 
         // Assert
         Assert.NotNull(result);
-        Assert.Equal("hjkhklh Item", result!.Name);
+        Assert.Equal("Test Item", result!.Name);
     }
 
     [Fact]
